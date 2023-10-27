ac98a3cc7701478345d8b5cc12f64b84bd72c892
Jishnu Teegala <134275562+jishnuteegala@users.noreply.github.com>
2023-10-27 17:14:10 +0100

    Add Python script file to generate QR Codes

---
+++ generate_qr_code_images.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import qrcode
+from PIL import Image
+
+# Function to generate QR code from contact details
+def generate_qr_code(data, output_file):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Save the QR code image
+    img.save(output_file)
+
+# Function to process Excel file and generate QR codes
+def generate_qr_codes_from_excel(input_excel_file, output_folder):
+    try:
+        df = pd.read_excel(input_excel_file)
+        for index, row in df.iterrows():
+            contact_data = f"Name: {row['Name']}\nEmail: {row['Email']}\nPhone: {row['Phone']}"
+            output_file = f"{output_folder}/{row['Name']}_qr.png"
+            generate_qr_code(contact_data, output_file)
+            print(f"QR code for {row['Name']} generated and saved as {output_file}")
+    except Exception as e:
+        print(f"Error processing the Excel file: {str(e)}")
+
+if __name__ == "__main__":
+    input_excel_file = r"C:\Users\YourUsername\Documents\contact_data.xlsx"  # Change this to your input Excel file
+    output_folder = r"C:\Users\YourUsername\Documents\QR_Codes"  # Change this to your desired output folder
+
+    generate_qr_codes_from_excel(input_excel_file, output_folder)
